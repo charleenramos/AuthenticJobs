d608360a2759ab62d5b084b76b64bf56221a62aa
charleenramos <oc.charleen@gmail.com>
2016-05-28 23:46:53 -0700

    Update jobScripts.js

--- jobScripts.js
+++ jobScripts.js
@@ -33,78 +33,12 @@ function doSearch(){
 	var currPage = load_more_button.data("result-page");
 	$("#busy").hide().html("<span class='spinner'>Loading...</span>").slideDown();
 	// build request URL
-	var _root = "https://authenticjobs.com/api/?api_key=308099f7b5001277edf90f3e98abd2a8&method=aj.jobs.search&keywords=php,mysql&perpage=1";//edited
-	var _key = "308099f7b5001277edf90f3e98abd2a8";//edited
+	var _root = "https://authenticjobs.com/api/?api_key=XXX&method=aj.jobs.search&keywords=php,mysql&perpage=1";//edited
+	var _key = "  ";//edited
 	var _ingredients = encodeURIComponent($("#ingredients").val());
 	var _page = currPage + 1;
 	var _url = _root + "?key=" + _key + "&q=" + _ingredients + "&page=" + _page;
 	var query = "select * from json where url=\"" + _url + "\""
 	makeRequest(_contents, query); // send and process HTTP request
 	load_more_button.data().result-page++; // update button
-}
-
-// handle CORS/JSONP AJAX requests via YQL
-function makeRequest(page, query){
-	$.getJSON(
-		"http://query.yahooapis.com/v1/public/yql",
-		{
-		q: query,
-		format: "json"
-		},
-		function(response, status){
-			if (status){ // process successful request
-				console.log(status);
-				var json = response.query.results;
-				console.log(json);
-				$.each(json, function(i, element){ // process each recipe
-					if (element){
-						if (element.count == 1){
-							processRecipe(page, element.recipes);
-						}
-						else if(element.count > 0){
-							var nextItemIndex = $("#contents li").size();
-							$.each(element.recipes, function(i, recipe){
-								processRecipe(page, recipe);
-							});
-							$($("#contents li").get(nextItemIndex)).scrollView();
-
-							$("#indicators").show();
-							$("#load_more").show().button("enable");
-						}
-						else if(element.count <= 0){
-							$("#contents").append($("<li class='center'><h2>NO RESULTS FOUND</h2><h5>Try modifying your search terms</h5><li>"));
-						}
-						if (element.count != 30) {
-							$("#indicators").slideUp();
-							return false;
-						}
-					}
-				});
-				$("#search_btn").button("enable");
-				$("#busy").slideUp();
-			}
-		}
-	).error(function(){
-		$("#indicators").slideUp();
-	});
-}
-
-// parse recipe and add to the page
-function processRecipe(page, recipe){
-	var newRecipe = $("<li><a target='_blank' href='" +
-		recipe.f2f_url + "'><img alt='" +
-		recipe.title + "' src='" +
-		recipe.image_url + "' /><h3>" +
-		recipe.title + "</h3><p> Publisher: " +
-		recipe.publisher + "</p></a></li>");
-	page.append(newRecipe).listview('refresh');
-}
-
-// auto scroll-to function
-$.fn.scrollView = function () {
-	return this.each(function () {
-		$('html, body').animate({
-		scrollTop: $(this).offset().top
-		}, 750);
-	});
 }
